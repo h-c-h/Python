137acc367619c7bb9e08e00cfa3b1d813851aa14
angelovegazava20@gmail.com <angelovegazava20@gmail.com>
2025-04-24 00:35:13 -0500

    Datos Simples

---
+++ Tipos de datos/datos_simples.py
@@ -0,0 +1,22 @@
+#Cadena de texto
+#Formas de usar String
+"string"
+'string'
+
+#Forma para escribir en multiples lines en string
+"""String
+string
+string"""
+'''String
+string
+string'''
+
+#int(numeros enteros)
+50
+
+#float(numeros con coma)
+50.3
+
+#bool(valores booleanos:True o False)
+True
+False
