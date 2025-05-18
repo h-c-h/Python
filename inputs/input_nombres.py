31bbd9772abda2702ee358fc5c683cae8a4746a9
angelovegazava20@gmail.com <angelovegazava20@gmail.com>
2025-05-17 23:28:14 -0500

    INPUT NOMBRES

---
+++ inputs/input_nombres.py
@@ -0,0 +1,10 @@
+#input
+#pedir un dato al usuario
+nombre = input("dame tu nombre: ")
+#mostrando el dato
+print("el nombre es: "+ nombre)
+#o
+print(f"el nombre es: {nombre}")
+#ahora si ponemos en nombre 11
+#este no vas a devolver un texto
+#input("text")->esto es un texto
